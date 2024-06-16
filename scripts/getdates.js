46302d0b9b0df0f0ad4aba22825a37c47f918a90
vincentogechi03@gmail.com <vincentogechi03@gmail.com>
2024-06-16 04:45:26 +0100

    week 01 assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Set the current year dynamically
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+
+    // Set the last modified date dynamically
+    const lastModified = document.lastModified;
+    document.getElementById('lastModified').textContent = 'Last modified: ' + lastModified;
+});
